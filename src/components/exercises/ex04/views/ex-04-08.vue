79c3d6868d6df550383cdec543b448e867191d20
ConorHawk <conornhawkins@gmail.com>
2018-04-26 08:46:05 +1200

    actually cant remember

--- src/components/exercises/ex04/views/ex-04-08.vue
+++ src/components/exercises/ex04/views/ex-04-08.vue
@@ -7,7 +7,7 @@
         <hr>
       </div>
       <div class="font-sans container mx-auto flex flex-wrap">
-        <div class="w-full sm:w-1/3 p-4 border-r border-grey flex justify-center flex-col ">
+        <div class="w-full sm:w-1/3 p-4 border-r border-grey flex justify-start flex-col ">
           <h2 class="font-normal pb-2">Employee anchor</h2>
           <p>Describe the Targeted Audience. Describe the characteristics and behaviors of the customer. The description must be succinct enough to fit on one page. It must also be descriptive enough to lead the development of the ANCHOR or the value of the offering.  In the ANCHOR tools, the targeted audience is never constrained to the current customer base. </p>
         </div>
--- src/components/module-header.vue
+++ src/components/module-header.vue
@@ -4,7 +4,7 @@
       <router-link class="font-lg text-white no-underline font-light" to="/"><i class="fas fa-caret-left"></i> Exit Tool</router-link>
     </div>
     <div class="hidden sm:flex w-1/3 justify-center">
-      <router-link style="transition: 0.2s all;" v-bind:class="{'bg-white': index <= ( currentPage - 1)}" v-for="(page, index) in pages" v-bind:key="'page' + index" class="mx-1 rounded-full h-4 w-4 flex items-center justify-center border border-white" :to="'/ex-01/' + (index + 1)"></router-link>
+      <router-link style="transition: 0.2s all;" v-bind:class="{'bg-white': index <= ( currentPage - 1)}" v-for="(page, index) in pages" v-bind:key="'page' + index" class="mx-1 rounded-full h-4 w-4 flex items-center justify-center border border-white" :to="'/'+ rootUrl +'/' + (index + 1)"></router-link>
     </div>
     <div class="text-white w-2/3 sm:w-1/3 text-right">
       <h1 class="text-lg font-normal pb-2">{{toolName}}</h1>
@@ -20,14 +20,17 @@ export default {
   data () {
     return {
       currentPage: 1,
-      pageTitle: ''
+      pageTitle: '',
+      rootUrl: ''
     }
   },
   methods: {
     getRouteInfo: function () {
       var url = this.$router.currentRoute.fullPath
+      var splitUrl = url.split('/')
       this.currentPage = parseInt(url.substring(url.lastIndexOf('/') + 1))
       this.pageTitle = this.$route.meta.pageTitle
+      this.rootUrl = splitUrl[1]
     }
   },
   mounted: function () {
